2a624a1f9518296a6a601ab3b34c0de37a56ccc5
henribru <6639509+henribru@users.noreply.github.com>
2021-09-28 15:44:26 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  {{ Foo }}
+  {{ Bar }}
+  {{ Baz }}
+</template>
+
+<script lang="ts">
+export enum Foo {
+  FOO = "Foo"
+}
+enum Bar {
+  BAR = "Bar"
+}
+</script>
+
+<script setup lang="ts">
+enum Baz {
+  BAZ = "Baz"
+}
+</script>
